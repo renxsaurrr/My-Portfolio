6d4e9a47c1e033157d94d05ee380b16bd9aa0fdd
Rendell Jhon Alfanta <r.alfanta.554091@umindanao.edu.ph>
2026-05-28 09:21:30 +0800

    remove queueable from contact mail

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -2,16 +2,12 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
 
 class ContactMail extends Mailable
 {
-    use Queueable, SerializesModels;
-
     public function __construct(public array $data) {}
 
     public function envelope(): Envelope
